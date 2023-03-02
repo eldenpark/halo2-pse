cd506e58e55c0c908f8c86f691b19d90da747b4e
Elden Park <eldeniyenden@gmail.com>
2023-03-02 16:06:30 -0700

    o

--- tree_maker/src/grow.rs
+++ tree_maker/src/grow.rs
@@ -1,8 +1,11 @@
 use crate::TreeMakerError;
 use aws_config::meta::region::RegionProviderChain;
 use aws_sdk_dynamodb::{client::fluent_builders, model::AttributeValue, Client as DynamoClient};
-use std::collections::HashMap;
-use tokio_postgres::{Client as PgClient, Error, NoTls};
+use futures_util::pin_mut;
+use std::{collections::HashMap, sync::Arc};
+// use tokio_postgres::types::ToSql;
+use futures_util::TryStreamExt;
+use tokio_postgres::{Client as PgClient, Error, GenericClient, NoTls};
 
 pub async fn grow_tree() -> Result<(), TreeMakerError> {
     let (pg_client, connection) = tokio_postgres::connect(
@@ -11,5 +14,42 @@ pub async fn grow_tree() -> Result<(), TreeMakerError> {
     )
     .await?;
 
+    let pg_client = Arc::new(pg_client);
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            println!("connection error: {}", e);
+        }
+    });
+
+    const NO_PARAMS: &[i32; 0] = &[];
+    let it = pg_client
+        .query_raw("SELECT count(*) as count from nodes", NO_PARAMS)
+        .await
+        .unwrap();
+
+    pin_mut!(it);
+
+    while let Some(row) = it.try_next().await? {
+        println!("222");
+        let foo: i64 = row.get(0);
+
+        println!("foo: {}", foo);
+    }
+
+    // let count: &str = leaves_count.Box::pin(0);
+
+    // println!("leaves_count: {:?}", count);
+
+    // for height in 0..32 {
+    //     let mut curr = 0;
+    //     while true {
+    //         pg_client.query(
+    //             "SELECT pos, table_id, val FROM nodes WHERE pos in ($1, $2)",
+    //             &[],
+    //         );
+
+    //         curr += 2;
+    //     }
+    // }
     Ok(())
 }
